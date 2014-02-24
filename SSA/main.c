e5bcc654ce451082fdd60eb43add6201008d8daf
Yun Yuan <yunyuan@YundeMacBook-Pro.local>
2014-02-24 16:43:42 +0800

    Initial Commit

---
+++ SSA/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  SSA
+//
+//  Created by Yun Yuan on 14-2-24.
+//  Copyright (c) 2014年 Yun Yuan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
